cff8e817c6231396786370a9a566de30b1cabfcb
lkhee <slaqmtm0735@gmail.com>
2025-03-11 22:29:05 +0900

    main startup 대체
    - @app.on_event("startup")가 Deprecated(사용 중단 예정)

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.company.router import router as company_router
 from app.database import init_db, wait_for_db
+import os
 
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup 대체
+    wait_for_db()
+    init_db()
+    yield
+    pass
 
 def create_app() -> FastAPI:
     """ app 변수 생성 및 초기값 설정 """
@@ -11,18 +21,10 @@ def create_app() -> FastAPI:
         title="LKH API",
         description="LKH FastAPI Swagger",
         version="1.0.0",
+        lifespan=lifespan  # lifespan 적용
     )
     _app.include_router(company_router, prefix="", tags=["COMPANY"])
     return _app
 
 app = create_app()
 
-
-# 앱 시작 시 데이터베이스 초기화
-@app.on_event("startup")
-async def startup():
-    wait_for_db()
-    init_db()
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
