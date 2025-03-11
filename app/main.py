297ea76a308960c859db174e787149bd0ab29f97
lkhee <slaqmtm0735@gmail.com>
2025-03-11 21:26:57 +0900

    main 수정

--- app/main.py
+++ app/main.py
@@ -1,10 +1,16 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from app.company.router import router as company_router
 from app.database import init_db, wait_for_db
 
 
+# 앱 시작 시 데이터베이스 초기화
+@asynccontextmanager
+async def startup(app: FastAPI):
+    wait_for_db()
+    init_db()
 
-def create_app() -> FastAPI:
+def create_app() -> FastAPI(startup=startup):
     """ app 변수 생성 및 초기값 설정 """
 
     _app = FastAPI(
@@ -17,12 +23,6 @@ def create_app() -> FastAPI:
 
 app = create_app()
 
-
-# 앱 시작 시 데이터베이스 초기화
-@app.on_event("startup")
-async def startup():
-    wait_for_db()
-    init_db()
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host='0.0.0.0', port=8000, reload=True)
